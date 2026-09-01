89c763facc0c4b53e68ac616e6bb2b2725da8a3a
Sahib Preet <sahibpreet761@gmail.com>
2026-09-01 22:46:29 +0530

    Day 22 : Question 44

---
+++ 100-Days-Of-Code/q44.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        if (i == 1) {
+            sum += 1;
+        } else {
+            sum += (float)(2 * i - 1) / (2 * i);
+        }
+    }
+
+    printf("Sum of the series = %.2f", sum);
+
+    return 0;
+}
