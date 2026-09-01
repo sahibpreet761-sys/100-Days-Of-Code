b45a4772f227f055158cc213e6edacbc2fe73b83
Sahib Preet <sahibpreet761@gmail.com>
2026-09-01 22:39:13 +0530

    Day 20 : Question 39

---
+++ 100-Days-Of-Code/q39.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, digit;
+    int product = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0) {
+        digit = n % 10;
+
+        if (digit % 2 != 0) {
+            product *= digit;
+        }
+
+        n = n / 10;
+    }
+
+    printf("Product of odd digits = %d", product);
+
+    return 0;
+}
