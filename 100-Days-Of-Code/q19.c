56dfa7f69b9af793251c60c201b4f02a42520d8d
Sahib Preet <sahibpreet761@gmail.com>
2026-09-01 21:48:31 +0530

    Day 10 : Question 19

---
+++ 100-Days-Of-Code/q19.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("Enter three sides of the triangle: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c)
+    {
+        printf("Equilateral Triangle");
+    }
+    else if (a == b || b == c || a == c)
+    {
+        printf("Isosceles Triangle");
+    }
+    else
+    {
+        printf("Scalene Triangle");
+    }
+
+    return 0;
+}
