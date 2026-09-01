a06c2083c0111e45e09f8fb36d43f176edbc8b5c
Sahib Preet <sahibpreet761@gmail.com>
2026-09-01 22:27:41 +0530

    Day 17 : Question 33

---
+++ 100-Days-Of-Code/q33.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, original, remainder, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0) {
+        remainder = n % 10;
+        sum += remainder * remainder * remainder;
+        n = n / 10;
+    }
+
+    if (sum == original) {
+        printf("%d is an Armstrong number.", original);
+    } else {
+        printf("%d is not an Armstrong number.", original);
+    }
+
+    return 0;
+}
